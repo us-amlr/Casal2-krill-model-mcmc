0d626d8ace8589e606fd3622af43e4802c7d734d
doug kinzey <64279035+dougkinzey@users.noreply.github.com>
2023-09-28 11:51:30 -0700

    Add files via upload
    Added 'combined non-simplex-0-catch' and 'casal2_dlls.zip' to repository

---
+++ combined non-simplex/combined non-simplex-0-catch/casal2_krill.bat
@@ -0,0 +1,7 @@
+cd C:\zot\Casal2\2023\9sep\sep26\simplex_0K_3spp\0K.c\biom
+casal2 -r >> krill.r.txt
+casal2 -e >> krill.e.txt
+casal2 -m >> krill_mcmc.m.txt 
+casal2 -r -i samples.1 -t >> krill_samples.txt
+casal2 -f 1 -i samples.1 -t >> projection.out.txt
+
